3d341b459851c137b2a6b0bc2650cda5838650ee
Nicolas Guerrero <nicolasf1402@gmail.com>
2020-05-25 17:25:55 -0500

    Update readme

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Platzi overflow
 
-```
-npm install
-```
+Sistema tipo foro, donde se permite iniciar sesió, crear y responder preguntas
+
+## Herramientas utilizadas
+
+- HapiJs
+- Hbs
--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const methods = require('./lib/methods')
 const routes = require('./routes')
 
 const server = Hapi.server({
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3001,
   host: 'localhost',
   routes: {
     files: {
